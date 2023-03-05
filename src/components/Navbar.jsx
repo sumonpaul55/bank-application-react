a7448d2c11d5a536b2f40ae557392000bb7ff8e8
sumon paul <sumpaul100@gmail.com>
2023-03-06 00:34:03 +0600

    navbar mobile menu working and we should use callback function for prev state update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { logo, close, menu } from "../assets";
 import { navLinks } from "../costants";
-console.log(navLinks);
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, settoggle] = useState(false);
+  return (
+    <nav className="w-full navbar text-white flex justify-between items-center py-6">
+      <img src={logo} alt="Hoobank" className="w-32 h-8" />
+      <ul className="list-none sm:flex hidden flex-1 justify-end">
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className={`font-poppins font-normal text-[16px] ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}>
+            <a className="no-underline" href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {/* mobile menu */}
+      <div className="flex-1 sm:hidden py-2 flex justify-end">
+        <img src={toggle ? close : menu} alt="menu" className="w-7 h-7 object-contain cursor-pointer" onClick={() => settoggle((prev) => !prev)} />
+      </div>
+      {/* menu */}
+      <div className={`${toggle ? "flex" : "hidden"} bg-black-gradient p-6 absolute top-20 right-0 mx-4 my-2 rounded-xl min-w-[140px] sidebar`}></div>
+    </nav>
+  );
 };
 
 export default Navbar;
