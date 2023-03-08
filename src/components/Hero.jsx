316f23893348708a6436d84780a3968437762fc4
sumon paul <sumpaul100@gmail.com>
2023-03-09 01:30:42 +0600

    work start hero or top section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import styles from "../styles";
+import { discount, robot } from "../assets";
+const Hero = () => (
+  <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center bg-discount-gradient rounded-[10px] mb-2">
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph}`}>
+          <span className="text-white"> 20%</span>
+          Discount for
+          <span className="text-white">1 Month</span>
+          <span className="">Account</span>
+        </p>
+      </div>
+    </div>
+  </section>
+);
 
 export default Hero;
