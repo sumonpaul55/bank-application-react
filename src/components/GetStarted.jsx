a5aa936d60896ad7f9e25c6ab613df0e84d0ad80
sumon paul <sumpaul100@gmail.com>
2023-03-19 23:21:18 +0600

    working on hero left

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "../styles";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return <div>GetStarted</div>;
-};
+const GetStarted = () => (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}>
+    <div className={`w-full h-full bg-black rounded-full ${styles.flexCenter} flex-col`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img src={arrowUp} alt="arrow up" />
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+);
 
 export default GetStarted;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import styles from "../styles";
+import GetStarted from "./GetStarted";
 import { discount, robot } from "../assets";
 const Hero = () => (
   <section id="home" className={`flex flex-col md:flex-row ${styles.paddingY}`}>
-    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center bg-discount-gradient rounded-[10px] mb-2">
+    <div className={`${styles.flexStart} flex-col w-full xl:px-0 sm:px-16 px-4`}>
+      <div className="flex flex-row items-center bg-discount-gradient rounded-[10px] mb-2 px-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph}`}>
-          <span className="text-white"> 20%</span>
-          Discount for
-          <span className="text-white">1 Month</span>
-          <span className="">Account</span>
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount for <span className="text-white">1 Month</span> Account
         </p>
       </div>
+      <div className="flex flex-row justify-between w-full">
+        <h1 className="text-white flex-1 text-[52px] ss:text-[72px] font-semibold font-poppins ss:leading-[100px] leading-[75px]">
+          The Next <br className="hidden sm:block" />
+          <span className="text-gradient">Generation</span>
+        </h1>
+        <div className="hidden ss:flex mr-0 ss:m-4 text-white">
+          <GetStarted />
+        </div>
+      </div>
+      <h1 className="text-white text-[52px] ss:text-[64px] font-semibold font-poppins ss:leading-[100px] leading-[75px] w-full">Payment Method.</h1>
+      <p className={`${styles.paragraph} max-w-[470px] mt-8`}>
+        Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.
+      </p>
+    </div>
+    <div className="w-full">
+      <img src={robot} alt="robot" className="w-[100%] h-[100%] z-[5]" />
     </div>
   </section>
 );
